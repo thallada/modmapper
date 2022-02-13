bb2059e6974c29c2b2147d7ce57f73c298ec6d0c
Tyler Hallada <tyler@hallada.net>
2022-02-12 23:26:45 -0500

    Fix find command options in backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,6 @@
 export $(grep -v '^#' .env | xargs -d '\n')
 mkdir -p backups
 zip -r -9 backups/plugins.zip plugins -DF --out backups/plugins-update.zip
-pg_dump -U modmapper -Fc modmapper > backups/modmapper-$(date +'%Y-%m-%d').dump
-find backups/modmapper-*.dump -mtime +30 --type f --delete
+pg_dump -h localhost -U modmapper -Fc modmapper > backups/modmapper-$(date +'%Y-%m-%d').dump
+find backups/modmapper-*.dump -mtime +30 -type f -delete
 rclone sync backups/* dropbox:modmapper
