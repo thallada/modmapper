4e5284a57f0786cb8f1e9fc0249b8335ccb62b64
Tyler Hallada <tyler@hallada.net>
2022-03-18 00:30:27 -0400

    Add mod_cell_counts dump to update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,5 +7,6 @@ mkdir -p plugins_data
 ./target/release/mod-mapper -e cells/edits.json
 ./target/release/mod-mapper -c cells
 ./target/release/mod-mapper -s mods/mod_search_index.json
+./target/release/mod-mapper -M mods/mod_cell_counts.json
 ./target/release/mod-mapper -m mods
 ./target/release/mod-mapper -P plugins_data
