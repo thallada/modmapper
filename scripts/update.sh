8f420c5558126f5e962684f1481c3f775fc66cd5
Tyler Hallada <tyler@hallada.net>
2022-06-04 16:48:10 -0400

    Pass -u option to plugins_data command, not cells

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,11 +10,11 @@ mkdir -p files
 mkdir -p plugins_data
 if [ -n "$last_update_time" ]; then
     ./target/release/mod-mapper -e cells/edits.json
-    ./target/release/mod-mapper -c cells -u "$last_update_time"
+    ./target/release/mod-mapper -c cells
     ./target/release/mod-mapper -s mods/mod_search_index.json
     ./target/release/mod-mapper -M mods/mod_cell_counts.json
     ./target/release/mod-mapper -m mods -u "$last_update_time"
-    ./target/release/mod-mapper -P plugins_data
+    ./target/release/mod-mapper -P plugins_data -u "$last_update_time"
     ./target/release/mod-mapper -F files -u "$last_update_time"
 else
     ./target/release/mod-mapper -e cells/edits.json
